615e7d17ba493f1809037b1b0791042cf684d4fa
Fernando Spark <fsostenes@outlook.com>
2024-12-16 14:35:02 -0600

    Add negative scenario for wrong method on GetBalance Endpoint

--- Betsson.OnlineWallets.API.Tests/src/test/GetBalanceTests/GetBalanceTests.cs
+++ Betsson.OnlineWallets.API.Tests/src/test/GetBalanceTests/GetBalanceTests.cs
@@ -11,6 +11,7 @@ public class GetBalanceTests
     private readonly String BASE_URL = "http://localhost:8087";
     private readonly String GET_BALANCE_ENDPOINT = "/onlinewallet/balance";
     private readonly String WIDTHDRAWAL_ENDPOINT = "/onlinewallet/withdraw";
+    private readonly String DEPOSIT_ENDPOINT = "/onlinewallet/deposit";
     private RestClient? restClient;
 
     [TestInitialize]
@@ -24,7 +25,7 @@ public class GetBalanceTests
     }
 
     [TestMethod]
-    public async Task GetBalanceAsync_GetInitialBalance_NoTransactions_BalanceZero()
+    public async Task GetBalanceAsync_GetInitialBalanceNoTransactions_BalanceZero()
     {
         MakeBalanceZero();
         var getBalancerequest = new RestRequest(GET_BALANCE_ENDPOINT);
@@ -37,7 +38,31 @@ public class GetBalanceTests
         Assert.AreEqual(expectedBalance, responseData.Amount);
     }
 
-    
+    [TestMethod]
+    public async Task GetBalanceAsync_GetBalanceAfterDeposit_CorrectBalance()
+    {
+        decimal depositAmount = 100;
+        MakeBalanceZero();
+        MakeDeposit(depositAmount);
+        var getBalancerequest = new RestRequest(GET_BALANCE_ENDPOINT);
+
+        var response = await restClient.GetAsync(getBalancerequest);
+        var responseData = JsonConvert.DeserializeObject<Balance>(response.Content);
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.AreEqual(depositAmount, responseData.Amount);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(HttpRequestException))]
+    public async Task GetBalanceAsync_WrongMethod_ErrorStatusCode()
+    {
+        var getBalancerequest = new RestRequest(GET_BALANCE_ENDPOINT);
+
+        var response = await restClient.PostAsync(getBalancerequest);
+
+        Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+    }
 
     private void MakeBalanceZero()
     {
@@ -52,6 +77,22 @@ public class GetBalanceTests
                 initialBalance.Amount
             });
             restClient.Post(withdrawalRequest);
+        }
+    }
+
+    private void MakeDeposit(decimal amount)
+    {
+        var getBalancerequest = new RestRequest(GET_BALANCE_ENDPOINT);
+        var initialBalance = restClient.Get<Balance>(getBalancerequest);
+        var depositRequest = new RestRequest(DEPOSIT_ENDPOINT);
+
+        if (initialBalance.Amount == 0)
+        {
+            depositRequest.AddJsonBody(new
+            {
+                Amount = amount
+            });
+            restClient.Post(depositRequest);
         }
     }
 }
