55f2dc1114fea254d98194eb66aec5bf3e92070f
Fernando Spark <fsostenes@outlook.com>
2024-12-16 14:05:25 -0600

    Adding Models for Balance and Withdrawal to assert on response values

---
+++ Betsson.OnlineWallets.API.Tests/src/Models/Balance.cs
@@ -0,0 +1,7 @@
+namespace Betsson.OnlineWallets.API.Tests.src.Models
+{
+    public class Balance
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Betsson.OnlineWallets.API.Tests/src/Models/Withdrawal.cs
@@ -0,0 +1,7 @@
+namespace Betsson.OnlineWallets.API.Tests.src.Models
+{
+    public class Withdrawal
+    {
+        public decimal Amount { get; set; }
+    }
+}
