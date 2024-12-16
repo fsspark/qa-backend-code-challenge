d54082951b02168634225222db18f066e0ca233a
Fernando Spark <fsostenes@outlook.com>
2024-12-16 01:41:01 -0600

    Modified BuildOnlineWalletEntry method

--- Betsson.OnlineWallets.Tests/src/test/GetBalanceTests/BetsonOnlineWalletService_GetBalanceTests.cs
+++ Betsson.OnlineWallets.Tests/src/test/GetBalanceTests/BetsonOnlineWalletService_GetBalanceTests.cs
@@ -9,35 +9,39 @@ using Moq;
 namespace Betsson.OnlineWallets.Tests.src.test.GetBalanceTests;
 
 [TestClass]
-public class BetssonOnlineWalletService_GetBalanceTests{
+public class BetssonOnlineWalletService_GetBalanceTests
+{
     private OnlineWalletService? onlineWalletService;
     private Mock<IOnlineWalletRepository>? mockOnlineWalletRepository;
 
     [TestInitialize]
-    public void Setup(){
+    public void Setup()
+    {
         mockOnlineWalletRepository = new Mock<IOnlineWalletRepository>();
         onlineWalletService = new OnlineWalletService(mockOnlineWalletRepository.Object);
     }
 
     [TestMethod]
-    public async Task GetBalanceAsync_NoTransactions_ReturnZero(){
+    public async Task GetBalanceAsync_NoTransactions_ReturnZero()
+    {
         //Arrange - Simulate no transactions
         mockOnlineWalletRepository
         .Setup(repo => repo.GetLastOnlineWalletEntryAsync())
-        .ReturnsAsync((OnlineWalletEntry)null); 
-        
+        .ReturnsAsync((OnlineWalletEntry)null);
+
         //Act
         Balance result = await onlineWalletService.GetBalanceAsync();
-        
+
         //Assert
         Assert.IsNotNull(result);
         Assert.AreEqual(0, result.Amount);
     }
 
     [TestMethod]
-    public async Task GetBalanceAsync_WithTransactions_ReturnCorrectBalance(){
+    public async Task GetBalanceAsync_WithTransactions_ReturnCorrectBalance()
+    {
         //Arrange
-        OnlineWalletEntry lastEntry = BuildOnlineEntry(50, 150);
+        OnlineWalletEntry lastEntry = BuildOnlineWalletEntry(50, 150);
         mockOnlineWalletRepository
         .Setup(walletRepository => walletRepository.GetLastOnlineWalletEntryAsync())
         .ReturnsAsync(lastEntry);
@@ -50,9 +54,10 @@ public class BetssonOnlineWalletService_GetBalanceTests{
     }
 
     [TestMethod]
-    public async Task GetBalanceASync_WithNegativeAmount_ReturnCorrectBalance(){
-                //Arrange
-        OnlineWalletEntry lastEntry = BuildOnlineEntry(-50, 150);
+    public async Task GetBalanceASync_WithNegativeAmount_ReturnCorrectBalance()
+    {
+        //Arrange
+        OnlineWalletEntry lastEntry = BuildOnlineWalletEntry(-50, 150);
         mockOnlineWalletRepository
         .Setup(walletRepository => walletRepository.GetLastOnlineWalletEntryAsync())
         .ReturnsAsync(lastEntry);
@@ -64,7 +69,8 @@ public class BetssonOnlineWalletService_GetBalanceTests{
         Assert.AreEqual(lastEntry.Amount + lastEntry.BalanceBefore, result.Amount);
     }
 
-    private OnlineWalletEntry BuildOnlineEntry(int balanceBefore, int amount){
+    private static OnlineWalletEntry BuildOnlineWalletEntry(int balanceBefore, int amount)
+    {
         Random rnd = new Random();
         OnlineWalletEntry onlineWalletEntry = new OnlineWalletEntry();
 
